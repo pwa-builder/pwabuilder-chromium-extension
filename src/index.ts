86ee9203408469df9e33f751c7aa26e3196094b6
Nikola Metulev <nmetulev@users.noreply.github.com>
2022-03-22 08:32:04 -0700

    minor cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import { LitElement, html } from "lit";
+import {customElement} from 'lit/decorators.js';
+
 import './components/scanner';
+@customElement("pwa-extension")
+export class PwaExtension extends LitElement {
+
+  render() {
+    return html`
+        <h1>PWA Builder Extension</h1>
+        <pwa-scanner></pwa-scanner>`;
+  }
+
+}
