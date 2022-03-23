2ca9ee373a73a5de8d2a694ad41749a642ddb765
Nikola Metulev <nmetulev@users.noreply.github.com>
2022-03-23 10:36:48 -0700

    added tabs

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,43 @@ import {customElement} from 'lit/decorators.js';
 import './components/scanner';
 import './components/package-windows';
 
+import {
+  provideFluentDesignSystem,
+  fluentTabs,
+  fluentTab,
+  fluentTabPanel
+} from "@fluentui/web-components";
+
+provideFluentDesignSystem().register(
+  fluentTabs(),
+  fluentTab(),
+  fluentTabPanel()
+);
+
 
 @customElement("pwa-extension")
 export class PwaExtension extends LitElement {
 
   render() {
     return html`
-      <h1>PWA Builder Extension</h1>
-      <pwa-scanner></pwa-scanner>`
+    <fluent-tabs>
+      <fluent-tab id="validate">Validate</fluent-tab>
+      <fluent-tab id="manifest">Manifest</fluent-tab>
+      <fluent-tab id="package">Package</fluent-tab>
+
+      <fluent-tab-panel id="validatePanel">
+        <pwa-scanner></pwa-scanner>
+      </fluent-tab-panel>
+
+      <fluent-tab-panel id="validatePanel">
+        Manifest Editor
+      </fluent-tab-panel>
+
+      <fluent-tab-panel id="validatePanel">
+        Package
+      </fluent-tab-panel>
+    </fluent-tabs>
+    `
     ;
   }
 
