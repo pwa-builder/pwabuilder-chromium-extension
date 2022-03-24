4c4b87444dd74fda250ad09d431a476aeceab2de
Justin Willis <jgw9617@gmail.com>
2022-03-24 12:18:26 -0700

    docs and cleanup

--- src/components/package-windows.ts
+++ src/components/package-windows.ts
@@ -9,10 +9,16 @@ import {
   provideFluentDesignSystem,
   fluentTextField,
   fluentButton,
-  fluentTooltip
+  fluentTooltip,
+  fluentAnchor,
 } from "@fluentui/web-components";
 
-provideFluentDesignSystem().register(fluentTextField(), fluentButton(), fluentTooltip());
+provideFluentDesignSystem().register(
+  fluentTextField(),
+  fluentButton(),
+  fluentTooltip(),
+  fluentAnchor()
+);
 
 @customElement("package-windows")
 export class PackageWindows extends LitElement {
@@ -44,6 +50,12 @@ export class PackageWindows extends LitElement {
       fluent-tooltip p {
         white-space: pre-wrap;
         width: 20em;
+      }
+
+      #header-block {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
       }
     `,
   ];
@@ -178,12 +190,18 @@ export class PackageWindows extends LitElement {
 
   render() {
     return html`
-      <h1>Package for The Microsoft Store</h1>
+      <div id="header-block">
+        <h1>Package for The Microsoft Store</h1>
+        <fluent-anchor href="https://blog.pwabuilder.com/docs/windows-platform/">Documentation</fluent-anchor>
+      </div>
 
       <form @submit="${($event: SubmitEvent) => this.handleSubmit($event)}">
         <!-- label for packageId input -->
         <fluent-tooltip id="tooltip" anchor="packageId">
-          <p>The Package ID uniquely identifying your app in the Microsoft Store. Get this value from Windows Partner Center.</p>
+          <p>
+            The Package ID uniquely identifying your app in the Microsoft Store.
+            Get this value from Windows Partner Center.
+          </p>
         </fluent-tooltip>
 
         <label for="packageId"
@@ -203,7 +221,10 @@ export class PackageWindows extends LitElement {
 
         <!-- label for publisherDisplayName input -->
         <fluent-tooltip id="tooltip" anchor="publisherDisplayName">
-          <p>The display name of your app's publisher. Get this value from Windows Partner Center.</p>
+          <p>
+            The display name of your app's publisher. Get this value from
+            Windows Partner Center.
+          </p>
         </fluent-tooltip>
         <label for="publisherDisplayName"
           >Publisher Display Name
@@ -220,7 +241,10 @@ export class PackageWindows extends LitElement {
 
         <!-- label for publisherCommonName input -->
         <fluent-tooltip id="tooltip" anchor="publisherCommonName">
-          <p>The ID of your app's publisher. Get this value from Windows Partner Center.</p>
+          <p>
+            The ID of your app's publisher. Get this value from Windows Partner
+            Center.
+          </p>
         </fluent-tooltip>
         <label for="publisherCommonName"
           >Publisher Common Name
@@ -240,7 +264,11 @@ export class PackageWindows extends LitElement {
 
         <!-- label for version input -->
         <fluent-tooltip id="tooltip" anchor="version">
-          <p>Your app version in the form of '1.0.0'. It must not start with zero and must be greater than classic package version. For new apps, this should be set to 1.0.1</p>
+          <p>
+            Your app version in the form of '1.0.0'. It must not start with zero
+            and must be greater than classic package version. For new apps, this
+            should be set to 1.0.1
+          </p>
         </fluent-tooltip>
 
         <label for="version"
@@ -259,7 +287,11 @@ export class PackageWindows extends LitElement {
 
         <!-- label for classicVersion input -->
         <fluent-tooltip id="tooltip" anchor="classicVersion">
-          <p>The version of your app that runs on older versions of Windows. Must be in the form of '1.0.0', it cannot start with zero, and must be less than app version. For new apps, this should be set to 1.0.0</p>
+          <p>
+            The version of your app that runs on older versions of Windows. Must
+            be in the form of '1.0.0', it cannot start with zero, and must be
+            less than app version. For new apps, this should be set to 1.0.0
+          </p>
         </fluent-tooltip>
         <label for="classicVersion"
           >Classic Version
